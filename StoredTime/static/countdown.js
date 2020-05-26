5e6aa3fe47bdab0c17ec7e92216b2405a1b6b99c
Alexander Anthis <anthisalex@gmail.com>
2020-05-25 17:13:52 -0700

    Created timer script

--- StoredTime/static/countdown.js
+++ StoredTime/static/countdown.js
@@ -1,12 +1,19 @@
 // imports
 const stopButton = document.getElementById('stop-button');
+const timeText = document.getElementById('time-text');
 
 // listeners
 window.addEventListener('load', function() {
-  askNotificationPermission();
+  timer();
 })
 
-stopButton.addEventListener('click', sendNotificiation('hello'))
+// vars
+
+var ct = { // initial values in case values are empty
+  hour: 0,
+  minute: 5,
+  second: 0
+}
 
 // functions
 function sendNotificiation(text) {
@@ -22,41 +29,67 @@ function sendNotificiation(text) {
   var notify = new Notification(notifyText);
 }
 
-//
-// These notification functions have been copied from Mozilla
-//
-function checkNotificationPromise() {
-    try {
-      Notification.requestPermission().then();
-    } catch(e) {
-      return false;
-    }
-
-    return true;
+function overrideDefaultTime(hour, minute, second) {
+  // overrides default values set above
+  if (hour != null && minute != null && second != null) {
+    ct.hour = hour;
+    ct.minute = minute;
+    ct.second = second;
+  }
 }
 
-function askNotificationPermission() {
-  // function to actually ask the permissions
-  function handlePermission(permission) {
-    // Whatever the user answers, we make sure Chrome stores the information
-    if(!('permission' in Notification)) {
-      Notification.permission = permission;
-    }
+function updateTimer(time) {
+  // Updates what's on screen with the timer
+  console.log(`Updating display timer to ${time.hour}:${time.minute}:${time.second}.`);
 
-  // Let's check if the browser supports notifications
-  if (!('Notification' in window)) {
-    console.log("This browser does not support notifications.");
+  // js stuff
+}
+
+function decreaseTimer() {
+  // Basic time decreaser
+
+  // Make sure the time is above 0
+  if (ct.hour < 0 || ct.minute < 0 || ct.second < 0) {
+    alert("Can't start a negative time!");
+    throw "Can't start a negative time!";
   } else {
-    if(checkNotificationPromise()) {
-      Notification.requestPermission()
-      .then((permission) => {
-        handlePermission(permission);
-      })
-    } else {
-      Notification.requestPermission(function(permission) {
-        handlePermission(permission);
-      })
+    // if second isn't 0, then just decrease the second
+    // by 1. otherwise, decrease the minute and hour if needed
+    if (ct.second == 0) {
+      if (ct.minute != 0) {
+        ct.minute = ct.minute - 1;
+        ct.second = 59;
+      }
+      else {
+        if (ct.hour != 0) {
+          ct.hour = ct.hour - 1;
+          ct.minute = 59;
+          console.log(ct.hour, ct.minute);
+        }
+        else {throw '';}
       }
     }
+    else {
+      ct.second = ct.second - 1;
+    }
+
+    console.log(`${ct.hour}:${ct.minute}:${ct.second}`);
+
+    updateTimer(ct);
   }
+}
+
+function timer(hour, minute, second) {
+  // TODO - For each timer object, run the timer for that object.
+
+  overrideDefaultTime(hour, minute, second);
+  updateTimer(ct);
+  var interval = setInterval(function() {
+    try {
+      decreaseTimer();
+    } catch (e) {
+      clearInterval(interval);
+      setTimeout(function() { alert(`Time is up! ${e}`); }, 1);
+    }
+   }, 1000);
 }
